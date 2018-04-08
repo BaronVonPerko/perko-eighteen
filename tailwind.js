2dbc69451e1f95c49f5026cb91343a6890ad2c13
Chris Perko <chris@chrisperko.net>
2018-04-08 14:54:08 -0400

    Hero section

--- tailwind.js
+++ tailwind.js
@@ -208,6 +208,7 @@ module.exports = {
       'sans-serif',
     ],
     'serif': [
+      'Open Sans',
       'Constantia',
       'Lucida Bright',
       'Lucidabright',
--- views/partials/front-page/hero.php
+++ views/partials/front-page/hero.php
@@ -1,1 +1,12 @@
-<h1>this is a test from the hero partial</h1>
+<div class="hero font-open-sans text-2xl leading-normal text-center relative">
+    <div class="absolute l-20 t-20">
+        <h1>&lt;Chris<span class="text-grey">PERKO</span>/&gt;</h1>
+        <h2 class="font-pacifico text-grey">Web Design and Development</h2>
+    </div>
+    <div>
+        <img 
+            class="absolute pin-r pin-t max-w-sm"
+            src="<?= get_template_directory_uri() . '/assets/images/apartments.jpg'?>" 
+            alt="">
+    </div>
+</div>
