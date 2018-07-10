23e3cf6332d4338a4b14742365ed300ce1d9459c
Chris Perko <chris@chrisperko.net>
2018-07-10 01:29:24 -0400

    Videos are now a CPT

--- inc/Custom/Custom.php
+++ inc/Custom/Custom.php
@@ -25,21 +25,21 @@ class Custom
 	public function custom_post_type() 
 	{
 		$labels = array(
-			'name'               => _x( 'Books', 'post type general name', 'Perko' ),
-			'singular_name'      => _x( 'Book', 'post type singular name', 'Perko' ),
-			'menu_name'          => _x( 'Books', 'admin menu', 'Perko' ),
-			'name_admin_bar'     => _x( 'Book', 'add new on admin bar', 'Perko' ),
-			'add_new'            => _x( 'Add New', 'book', 'Perko' ),
-			'add_new_item'       => __( 'Add New Book', 'Perko' ),
-			'new_item'           => __( 'New Book', 'Perko' ),
-			'edit_item'          => __( 'Edit Book', 'Perko' ),
-			'view_item'          => __( 'View Book', 'Perko' ),
-			'view_items'         => __( 'View Books', 'Perko' ),
-			'all_items'          => __( 'All Books', 'Perko' ),
-			'search_items'       => __( 'Search Books', 'Perko' ),
-			'parent_item_colon'  => __( 'Parent Books:', 'Perko' ),
-			'not_found'          => __( 'No books found.', 'Perko' ),
-			'not_found_in_trash' => __( 'No books found in Trash.', 'Perko' )
+			'name'               => _x( 'Videos', 'post type general name', 'Perko' ),
+			'singular_name'      => _x( 'Video', 'post type singular name', 'Perko' ),
+			'menu_name'          => _x( 'Videos', 'admin menu', 'Perko' ),
+			'name_admin_bar'     => _x( 'Video', 'add new on admin bar', 'Perko' ),
+			'add_new'            => _x( 'Add New', 'video', 'Perko' ),
+			'add_new_item'       => __( 'Add New Video', 'Perko' ),
+			'new_item'           => __( 'New Video', 'Perko' ),
+			'edit_item'          => __( 'Edit Video', 'Perko' ),
+			'view_item'          => __( 'View Video', 'Perko' ),
+			'view_items'         => __( 'View Videos', 'Perko' ),
+			'all_items'          => __( 'All Videos', 'Perko' ),
+			'search_items'       => __( 'Search Videos', 'Perko' ),
+			'parent_item_colon'  => __( 'Parent Videos:', 'Perko' ),
+			'not_found'          => __( 'No videos found.', 'Perko' ),
+			'not_found_in_trash' => __( 'No videos found in Trash.', 'Perko' )
 		);
 
 		$args = array(
@@ -49,9 +49,9 @@ class Custom
 			'publicly_queryable' => true,
 			'show_ui'            => true,
 			'show_in_menu'       => true,
-			'menu_icon'          => 'dashicons-book-alt',
+			'menu_icon'          => 'dashicons-video-alt3',
 			'query_var'          => true,
-			'rewrite'            => array( 'slug' => 'book' ),
+			'rewrite'            => array( 'slug' => 'video' ),
 			'capability_type'    => 'post',
 			'has_archive'        => true,
 			'hierarchical'       => false,
@@ -59,7 +59,7 @@ class Custom
 			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
 		);
 
-		register_post_type( 'book', $args );
+		register_post_type( 'video', $args );
 	}
 
 	/**
