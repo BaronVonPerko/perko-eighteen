862ef2de748f7a1a995bbeb5329d2bc9947ff0c8
Chris Perko <christopher.j.perko@windstream.com>
2018-07-12 19:00:44 -0400

    Custom WP Query to grab the posts

--- page-blog.php
+++ page-blog.php
@@ -1,1 +1,19 @@
 <?php get_header(); ?>
+
+<?php
+
+$GLOBALS['wp_query'] = new WP_Query( array(
+	'posts_per_page' => 5,
+	'paged'          => true,
+) );
+
+if ( have_posts() ) :
+
+	while ( have_posts() ) :
+		the_post();
+		get_template_part( 'views/content', get_post_format() );
+	endwhile;
+
+	the_posts_pagination();
+
+endif;
