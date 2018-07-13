f58133e90b9825366ede29f827bf895f9dc4b574
Chris Perko <christopher.j.perko@windstream.com>
2018-07-12 21:04:04 -0400

    Entire tile is clickable

--- page-blog.php
+++ page-blog.php
@@ -10,17 +10,17 @@ $GLOBALS['wp_query'] = new WP_Query( array(
 ?>
 
 <?php if ( have_posts() ) : ?>
-<div class="container my-8">
-    <div class="flex flex-wrap justify-center">
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			get_template_part( 'views/content-blog-tile', get_post_format() );
-		endwhile;
-		?>
-    </div>
+    <div class="container my-8">
+        <div class="flex flex-wrap justify-center">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'views/content-blog-tile', get_post_format() );
+			endwhile;
+			?>
+        </div>
 
-	<?php the_posts_pagination(); ?>
-</div>
+		<?php the_posts_pagination(); ?>
+    </div>
 
 <?php endif; ?>
--- views/content-blog-tile.php
+++ views/content-blog-tile.php
@@ -17,15 +17,17 @@
     }
 </style>
 
-<article id="post-<?= the_ID() ?>" <?php post_class( 'w-1/4 h-48 m-8 relative shadow-lg' ); ?>>
+<a class="w-1/4 h-48 m-8 relative shadow-lg" rel="bookmark" href="<?= esc_url( get_permalink() ) ?>">
+    <article id="post-<?= the_ID() ?>" <?php post_class(); ?>>
 
-    <div class="post-tile-image absolute pin"></div>
+        <div class="post-tile-image absolute pin"></div>
 
-    <header class="entry-header absolute pin-b w-full" style="background-color: rgba(40,40,40,0.7)">
-		<?php the_title(
-			'<h2 class="entry-title py-2 px-4"><a class="text-grey-lightest text-base font-light no-underline" href="' . esc_url( get_permalink() ) . '" rel="bookmark">',
-			'</a></h2>'
-		); ?>
-    </header><!-- .entry-header -->
+        <header class="entry-header absolute pin-b w-full" style="background-color: rgba(40,40,40,0.7)">
+			<?php the_title(
+				'<h2 class="entry-title py-2 px-4 text-grey-lightest text-base font-light no-underline">',
+				'</h2>'
+			); ?>
+        </header><!-- .entry-header -->
 
-</article><!-- #post-## -->
+    </article><!-- #post-## -->
+</a>
