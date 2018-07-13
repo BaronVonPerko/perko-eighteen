7339377409483f659ede465128e4e2999d1303f3
Chris Perko <christopher.j.perko@windstream.com>
2018-07-12 20:37:44 -0400

    WIP post tile styling

--- views/content-blog-tile.php
+++ views/content-blog-tile.php
@@ -9,48 +9,25 @@
 ?>
 
 <style>
-    #post-<?=the_ID()?> .post-thumbnail {
+    #post-<?=the_ID()?> .post-tile-image {
         background-image: url(<?php the_post_thumbnail_url() ?>);
-        width: 300px;
-        height: 300px;
+        width: 100%;
+        height: 100%;
         -webkit-background-size: cover;
         background-size: cover;
         background-position: center center;
     }
 </style>
 
-<article id="post-<?= the_ID() ?>" <?php post_class('w-1/3'); ?>>
+<article id="post-<?= the_ID() ?>" <?php post_class( 'w-1/3 h-48 relative' ); ?>>
+
+    <div class="post-tile-image absolute pin" style="z-index: -1;"></div>
+
     <header class="entry-header">
 		<?php the_title(
 			'<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">',
 			'</a></h2>'
 		); ?>
-
-        <div class="entry-meta">
-			<?php Perko\Core\Tags::posted_on(); ?>
-        </div><!-- .entry-meta -->
-
     </header><!-- .entry-header -->
 
-    <div class="entry-content">
-
-        <div class="post-thumbnail"></div>
-
-        <div>
-			<?php the_excerpt(); ?>
-        </div>
-    </div><!-- .entry-content -->
-
-    <div class="link-pages">
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Perko' ),
-			'after'  => '</div>',
-		) );
-		?>
-    </div>
-
-    <footer class="entry-footer">
-		<?php Perko\Core\Tags::entry_footer(); ?>
-    </footer><!-- .entry-footer -->
 </article><!-- #post-## -->
