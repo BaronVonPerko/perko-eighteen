e601d01e5f6e2fae07c00d354c3800abc536d8fc
Chris Perko <chris@chrisperko.net>
2018-07-21 15:39:16 -0400

    Added latest videos to blog page

--- page-blog.php
+++ page-blog.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
+<h3 class="blog_page_title">Latest Posts</h3>
+
 <?php
 
 $GLOBALS['wp_query'] = new WP_Query( array(
@@ -21,6 +23,36 @@ $GLOBALS['wp_query'] = new WP_Query( array(
         </div>
 
 		<?php the_posts_pagination(); ?>
+    </div>
+
+<?php endif; ?>
+
+
+<h3 class="blog_page_title">Latest Videos</h3>
+
+
+<?php
+
+wp_reset_query();
+
+$GLOBALS['wp_query'] = new WP_Query( array(
+	'posts_per_page' => 3,
+	'paged'          => false,
+	'post_type'      => [ 'video' ],
+) );
+
+?>
+
+<?php if ( have_posts() ) : ?>
+    <div class="container my-8">
+        <div class="flex flex-wrap justify-center">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'views/content-blog-tile', get_post_format() );
+			endwhile;
+			?>
+        </div>
     </div>
 
 <?php endif; ?>
